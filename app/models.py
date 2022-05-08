fcfe7300db588f6c6d735d7c7cc363fa0c7594a0
Sohrabbeig <ahsohrabbeig@gmail.com>
2022-05-08 00:19:33 +0000

    migration

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    points = db.Column(db.Integer)
+    soc = db.Column(db.Integer)
 
     def __repr__(self) -> str:
         return "<User {}>".format(self.username)
